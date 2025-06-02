34e3849c13ee67bea634b5bb636ce23c07563b2d
Manvith Rai <manvithrai16@gmail.com>
2025-06-02 13:05:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
